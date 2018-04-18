57829524d950211ab3e119b1d7eced22ecf55d47
leoiphonedev <aggarwalaman1985@gmail.com>
2018-04-18 23:31:55 +0530

    Pushing code for the tutorial Google Place Search API

--- GooglePlaceAPI/ViewController.swift
+++ GooglePlaceAPI/ViewController.swift
@@ -8,13 +8,98 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var tblPlaces: UITableView!
+    var resultsArray:[Dictionary<String, AnyObject>] = Array()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtSearch.addTarget(self, action: #selector(searchPlaceFromGoogle(_:)), for: .editingChanged)
+        tblPlaces.estimatedRowHeight = 44.0
+        tblPlaces.dataSource = self
+        tblPlaces.delegate = self
+    }
+    
+    //MARK:- UITableViewDataSource and UItableViewDelegates
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "placecell")
+        if let lblPlaceName = cell?.contentView.viewWithTag(102) as? UILabel {
+            
+            let place = self.resultsArray[indexPath.row]
+            lblPlaceName.text = "\(place["name"] as! String) \(place["formatted_address"] as! String)"
+        }
+        return cell!
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let place = self.resultsArray[indexPath.row]
+        if let locationGeometry = place["geometry"] as? Dictionary<String, AnyObject> {
+            if let location = locationGeometry["location"] as? Dictionary<String, AnyObject> {
+                if let latitude = location["lat"] as? Double {
+                    if let longitude = location["lng"] as? Double {
+                        UIApplication.shared.open(URL(string: "https://www.google.com/maps/@\(latitude),\(longitude),16z")!, options: [:])
+                    }
+                }
+            }
+        }
     }
 
+    
+    
+   @objc func searchPlaceFromGoogle(_ textField:UITextField) {
+    
+    if let searchQuery = textField.text {
+        var strGoogleApi = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(searchQuery)&key= AIzaSyABWLJXK3XYoNT3-1A7yQmXMLuu03Oln_Y"
+        strGoogleApi = strGoogleApi.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        var urlRequest = URLRequest(url: URL(string: strGoogleApi)!)
+        urlRequest.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, resopnse, error) in
+            if error == nil {
+                
+                if let responseData = data {
+                let jsonDict = try? JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                    
+                    if let dict = jsonDict as? Dictionary<String, AnyObject>{
+                        
+                        if let results = dict["results"] as? [Dictionary<String, AnyObject>] {
+                            print("json == \(results)")
+                            self.resultsArray.removeAll()
+                            for dct in results {
+                                self.resultsArray.append(dct)
+                            }
+                            
+                            DispatchQueue.main.async {
+                             self.tblPlaces.reloadData()
+                            }
+                            
+                        }
+                    }
+                   
+                }
+            } else {
+                //we have error connection google api
+            }
+        }
+        task.resume()
+    }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
